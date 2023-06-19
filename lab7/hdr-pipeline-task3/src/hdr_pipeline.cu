44624509ae66790db95aa67ed91596e6c08b5665
dak4408 <dak4408@l-g308-18a21.rz.fh-ingolstadt.de>
2023-06-19 11:19:25 +0200

    blurr kernel fast fertig

--- lab7/hdr-pipeline-task3/src/hdr_pipeline.cu
+++ lab7/hdr-pipeline-task3/src/hdr_pipeline.cu
@@ -65,12 +65,12 @@ __global__ void log_avg_lum_kernel(const float* in, int width, int height) {
     }
 }
 
-__device__ float calc_fade_contribution(float3 v, float exposure, float threshold){
-    
-    
+__device__ float square(float x){
+    return x * x;
+}
 
-    float res = powf(saturate((tonemap(v, exposure) - 0.8f * threshold) / 0.2f * threshold), 2);
-    return res;
+__device__ float bright_pass_weight(float color, float exposure, float threshold){
+    return square(((tonemap(color, exposure) - 0.8f * threshold) / 0.2f * threshold));
 }
 
 float saturate(float x){
@@ -86,15 +86,68 @@ float saturate(float x){
 }
 
 
-void bright_pass_x_y(float exposure, const float* in){
-    const float threshold = 1.0f;
-    // todo: cuda e
-    calc_fade_contribution(in, exposure, threshold) * in;
+
+__device__ float brightpass(float value, float exposure, float threshold){
+    return value * bright_pass_weight(value, exposure, threshold);
 }
 
+__global__ void bright_pass_kernel(uint32_t* out, const float* in, int width, int height, float exposure, float threshold){
+
+    const unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    if(x < width && y < height){
+        float3 color = {
+            brightpass(in[4 * (y * width + x) + 0], exposure, threshold),
+            brightpass(in[4 * (y * width + x) + 1], exposure, threshold),
+            brightpass(in[4 * (y * width + x) + 2], exposure, threshold)
+        };
+        uint32_t srgb = sRGB8(tonemap(color, exposure));
+        out[y*width + x] = srgb;
+    }
+}
+
+__device__ float compute_blur(float weight, float x, float y, float exposure, float threshold){
+    float value = 0.0f; // TODO: Float value
+    return weight * brightpass(value, exposure, threshold);
+}
+
+__global__ void blurr_kernel_x(uint32_t* out, const float* in, int width, int height, float exposure, float threshold){
+    const unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+    
+    float blurr_x = 0.0f;
+
+    if(x < width && y < height){
+        float3 color = {
+
+        };
+        for (int i = -31; i < 31; ++i){
+            blurr_x +=compute_blur(bloom_kernel[i + 31], x + 1, y, exposure, threshold);
+        }
+
+        out[x * width] = blurr_x;
+    }
+}
+
+// blur xy -> speicher allozieren
+__global__ void blurr_kernel_y(uint32_t* out, const float* in, int width, int height, float exposure, float threshold){
+    const unsigned int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const unsigned int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    float blurr_y = 0.0f;
+
+    if(x < width && y < height){
+        float brightpass = 0.0f; // : compute brightpass  
+        for (int i = -31; i < 32; ++i){
+            blurr_y = compute_blur(bloom_kernel[i + 31], x, y + i, exposure, threshold);
+        }
+        out[y * height] = blurr_y;
+    }
+}
 
 void blurr(){
-
+    // blurr_x * blurr_y
 }
 
 void tonemap(uint32_t* out, const float* in, int width, int height, float exposure, float brightpass_threshold) {
@@ -120,7 +173,6 @@ float compute_avg_luminance(const float* in, int width, int height) {
     cudaMemcpyToSymbol(log_avg_lum, &luminance, sizeof(float));
 
     log_avg_lum_kernel<<<num_blocks, block_size>>>(in, width, height);
-
     cudaMemcpyFromSymbol(&luminance, log_avg_lum, sizeof(float));
 
     return std::exp(luminance / (width * height));
